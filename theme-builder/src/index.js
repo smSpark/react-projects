1e7055aac3e3667973d9c545a41a0d197dc28d56
smSpark <you@gmail.com>
2021-02-02 02:21:33 +0300

    theme-project

---
+++ theme-builder/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import * as themes from './theme/schema.json'
+import { setToLS } from './utils/storage'
+
+const Index = () => {
+  setToLS('all-themes', themes.default)
+  return <App />
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
